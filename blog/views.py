a6baa0c45a2ebf30dd7bffa247d647644e5b1caf
Michellecp <chelle.info@gmail.com>
2018-11-03 17:38:29 -0300

    Atualização da View

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 
+Post.objects.get(pk=pk)
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
